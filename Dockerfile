c0668b419786001c8d4924afdab1f831e4d06590
JeskaLopes <jessica97cantora@hotmail.com>
2023-05-04 16:18:57 -0300

    projeto integrador

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,4 @@ COPY --from=build ${DEPENDENCY}/BOOT-INF/lib /app/lib
 COPY --from=build ${DEPENDENCY}/META-INF /app/META-INF
 COPY --from=build ${DEPENDENCY}/BOOT-INF/classes /app
 
-ENTRYPOINT ["java","-cp","app:app/lib/","com.generation.plantanamesa.PlantaNaMesaApplication"]
+ENTRYPOINT ["java","-cp","app:app/lib/","com.generation.plantanamesa.PlantanamesaApplication"]
--- src/main/java/com/generation/plantanamesa/PlantanamesaApplication.java
+++ src/main/java/com/generation/plantanamesa/PlantanamesaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlantaNaMesaApplication {
+public class PlantanamesaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlantaNaMesaApplication.class, args);
+		SpringApplication.run(PlantanamesaApplication.class, args);
 	}
 
 }
